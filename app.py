7f8e8b2fb6853ef16b8a452f1b1faf15adbdadfb
hellokitty <ts2447072778@163.com>
2020-02-24 20:55:06 +0800

    完成主页渲染

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask
+import os
+
+from flask import Flask,render_template
+# from flask_sqlalchemy import SQLAlchemy
 app =Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "欢迎大家来到我的项目"
+def index():
+    name = "Bruce"
+    movies = [
+        {'title': '杀破狼', 'year': '2003'},
+        {'title': '扫毒', 'year': '2018'},
+        {'title': '捉妖记', 'year': '2016'},
+        {'title': '囧妈', 'year': '2020'},
+        {'title': '葫芦娃', 'year': '1989'},
+        {'title': '玻璃盒子', 'year': '2020'},
+        {'title': '调酒师', 'year': '2020'},
+        {'title': '釜山行', 'year': '2017'},
+        {'title': '导火索', 'year': '2005'},
+        {'title': '叶问', 'year': '2015'}
+    ]
+
+    return render_template('index.html', name=name, movies=movies)
